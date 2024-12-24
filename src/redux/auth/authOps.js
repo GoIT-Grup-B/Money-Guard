55e0d8b539813bc8208d04fe69f68410d0523a56
Şubat Yücel <subat.yucel1@gmail.com>
2024-12-24 17:39:19 +0300

    redux fixed

--- src/redux/auth/authOps.js
+++ src/redux/auth/authOps.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const URL = 'https://wallet.b.goit.study/api';
 
 export const registerUser = createAsyncThunk(
-  'user/register',
+  'auth/register',
   async ({ username, email, password }, thunkAPI) => {
     try {
       const res = await axios.post(`${URL}/auth/sign-up`, {
@@ -15,13 +15,13 @@ export const registerUser = createAsyncThunk(
 
       return res.data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
+      return thunkAPI.rejectWithValue(error);
     }
   },
 );
 
 export const loginUser = createAsyncThunk(
-  'user/login',
+  'auth/login',
   async ({ email, password }, thunkAPI) => {
     try {
       const res = await axios.post(`${URL}/auth/sign-in`, {
@@ -31,7 +31,19 @@ export const loginUser = createAsyncThunk(
 
       return res.data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
+      return thunkAPI.rejectWithValue(error);
+    }
+  },
+);
+
+export const signOutUser = createAsyncThunk(
+  'auth/sign-out',
+  async (_, thunkAPI) => {
+    try {
+      await axios.delete(`${URL}/auth/sign-out`);
+      return {};
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
   },
 );
