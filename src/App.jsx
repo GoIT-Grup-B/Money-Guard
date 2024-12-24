7bb37c2d59766247055c990e7fe88ed9accebd48
Mete <metlandus@hotmail.com.tr>
2024-12-24 23:53:40 +0300

    Tailwind css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
+import './index.css';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
+import AddTransactionForm from './components/AddTransactionForm/AddTransactionForm';
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="register" element={<RegistrationPage />} />
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path="login" element={<LoginPage />} />
+                <Route path="register" element={<RegistrationPage />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
