deec58855ef137492afdffee423b21ab9ca4b5c7
gizemcoban <gizem.coban.r@gmail.com>
2024-12-24 19:42:16 +0300

    table structure was prepared

---
+++ src/pages/DashboardPage/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // Tailwind'in hangi dosyalarda kullanılacağını belirtir
+    ],
+    theme: {
+      extend: {}, // Tema özelleştirmeleri için
+    },
+    plugins: [],
+  };
+  
