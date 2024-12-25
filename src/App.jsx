575d829145fc18348584936e150b9db1f83f1fee
Heza GÖLCÜK <h.golcuk91@outlook.com>
2024-12-25 22:26:40 +0300

    headera başlandı

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { Route, Routes } from 'react-router-dom';
 import './index.css';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
+import DashboardPage from './pages/DashboardPage/DashboardPage';
 import AddTransactionForm from './components/AddTransactionForm/AddTransactionForm';
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route path="login" element={<LoginPage />} />
-                <Route path="register" element={<RegistrationPage />} />
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegistrationPage />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
