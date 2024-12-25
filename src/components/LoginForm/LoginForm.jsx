ccc880a6bed3a68a663c4caa2d5bcea47cc0b45c
Şubat Yücel <subat.yucel1@gmail.com>
2024-12-25 23:51:53 +0300

    some styles added to login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -25,28 +25,48 @@ function LoginForm() {
     handleSubmit,
     formState: { errors },
   } = useForm({ resolver: yupResolver(schema) });
-  // function handleSubmit() {}
 
   function onSubmit(data) {
     dispatch(loginUser(data));
   }
 
   return (
-    <div>
-      <img src={logo} alt="money guard logo" width="35px" />
-      <h1>Money Guard</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <input type="text" id="mail" {...register('email')} />
-          {errors.email && <p>{errors.email.message}</p>}
-        </div>
-        <div>
-          <input type="password" id="password" {...register('password')} />
-          {errors.password && <p>{errors.password.message}</p>}
-        </div>
-        <button type="submit">LOG IN</button>
-        <Link to="/register">REGISTER</Link>
-      </form>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="container mx-auto my-auto max-w-[533px] min-w-[320px] px-[20px] md:px-[62px] py-[98px] md:py[80px] bg-white/10 bg-blur-3xl">
+        <header className="flex flex-col justify-center items-center mb-[52px]">
+          <img src={logo} alt="money guard logo" width="35px" className="" />
+          <h1>Money Guard</h1>
+        </header>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-[40px]"
+        >
+          <div className="border-b-2 border-white/40 pb-2 px-2 flex gap-5">
+            <img src={logo} alt="" width="24px" />
+            <input
+              placeholder="E-mail"
+              type="text"
+              id="mail"
+              {...register('email')}
+              className="bg-transparent outline-none w-full"
+            />
+          </div>
+          <div className="border-b-2 border-white/40 pb-2 px-2 flex gap-5">
+            <img src={logo} alt="" width="24px" />
+            <input
+              placeholder="Password"
+              type="password"
+              id="password"
+              {...register('password')}
+              className="bg-transparent outline-none w-full"
+            />
+          </div>
+          <div className="flex flex-col items-center gap-[20px]">
+            <button type="submit">LOG IN</button>
+            <Link to="/register">REGISTER</Link>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
