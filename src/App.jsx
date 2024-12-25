8d6b6542dbb062b9abaca946f4307561bf533c27
Mete <metlandus@hotmail.com.tr>
2024-12-25 18:55:36 +0300

    Tailwind config optimized

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './index.css';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
-<<<<<<< HEAD
-import AddTransactionForm from './components/AddTransactionForm/AddTransactionForm';
+import DashBoardTable from './pages/DashboardPage/DashBoardTable';
 
 function App() {
     return (
@@ -11,24 +10,10 @@ function App() {
             <Routes>
                 <Route path="login" element={<LoginPage />} />
                 <Route path="register" element={<RegistrationPage />} />
+                <Route path="dashboard" element={<DashBoardTable />} />
             </Routes>
         </>
     );
-=======
-import DashBoardTable from './pages/DashboardPage/DashBoardTable';
-
-function App() {
-  return (
-    
-    <>
-      <Routes>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="register" element={<RegistrationPage />} />
-        <Route path="dashboard" element={<DashBoardTable/>} />
-      </Routes>
-    </>
-  );
->>>>>>> feature/add-transaction-gizem
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
     content: [
         './src/**/*.{js,jsx,ts,tsx}'
     ],
-=======
-  content: [],
->>>>>>> feature/add-transaction-gizem
   theme: {
     extend: {},
   },
