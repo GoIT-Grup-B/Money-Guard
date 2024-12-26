5c665a076e133be40c9ddd323db7a49eeb4cbc63
Şubat Yücel <subat.yucel1@gmail.com>
2024-12-26 12:35:21 +0300

    login styles done

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,10 +1,12 @@
-import { Link } from 'react-router-dom';
-import logo from '../../assets/svg/logo-login.svg';
 import * as yup from 'yup';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { loginUser } from '../../redux/auth/authOps';
 import { useDispatch } from 'react-redux';
+import FormHeader from '../FormHeader/FormHeader';
+import FormFooter from '../FormFooter/FormFooter';
+import mailSvg from '../../assets/svg/mail.svg';
+import passswordSvg from '../../assets/svg/password.svg';
 
 const schema = yup.object().shape({
   email: yup
@@ -31,18 +33,18 @@ function LoginForm() {
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div
+      className={`flex items-center justify-center min-h-screen bg-[url('/src/assets/img/login-desktop.webp')] bg-cover bg-center`}
+    >
       <div className="container mx-auto my-auto max-w-[533px] min-w-[320px] px-[20px] md:px-[62px] py-[98px] md:py[80px] bg-white/10 bg-blur-3xl">
-        <header className="flex flex-col justify-center items-center mb-[52px]">
-          <img src={logo} alt="money guard logo" width="35px" className="" />
-          <h1>Money Guard</h1>
-        </header>
+        <FormHeader />
+
         <form
           onSubmit={handleSubmit(onSubmit)}
           className="flex flex-col gap-[40px]"
         >
           <div className="border-b-2 border-white/40 pb-2 px-2 flex gap-5">
-            <img src={logo} alt="" width="24px" />
+            <img src={mailSvg} alt="" width="24px" />
             <input
               placeholder="E-mail"
               type="text"
@@ -52,7 +54,7 @@ function LoginForm() {
             />
           </div>
           <div className="border-b-2 border-white/40 pb-2 px-2 flex gap-5">
-            <img src={logo} alt="" width="24px" />
+            <img src={passswordSvg} alt="" width="24px" />
             <input
               placeholder="Password"
               type="password"
@@ -61,10 +63,7 @@ function LoginForm() {
               className="bg-transparent outline-none w-full"
             />
           </div>
-          <div className="flex flex-col items-center gap-[20px]">
-            <button type="submit">LOG IN</button>
-            <Link to="/register">REGISTER</Link>
-          </div>
+          <FormFooter type="login" />
         </form>
       </div>
     </div>
