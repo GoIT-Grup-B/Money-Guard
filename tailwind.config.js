0ccd09e6019a856ab9d1c3fa3af6be31563e5440
Mete <metlandus@hotmail.com.tr>
2024-12-27 01:29:46 +0300

    AddTransaction Modal added w/o func. Css created

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,18 @@ export default {
     content: [
         './src/**/*.{js,jsx,ts,tsx}'
     ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    theme: {
+        screens: {
+            'mobile': '370px',
+            // => @media (min-width: 370px) { ... }
+
+            'tablet': '768px',
+            // => @media (min-width: 768px) { ... }
+
+            'desktop': '1280px',
+            // => @media (min-width: 1280px) { ... }
+        },
+    },
+    plugins: [],
 }
 
