a65b3d6621489e704f0c3df140734c7f83003504
gizemcoban <gizem.coban.r@gmail.com>
2024-12-29 17:32:40 +0300

    gizem

--- src/pages/DashboardPage/DashBoardTable.jsx
+++ src/pages/DashboardPage/DashBoardTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './DashBoardTable.module.css';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
