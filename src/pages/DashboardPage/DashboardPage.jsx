a05511b4868c9b8f47dd4b9744cd321aeaceb228
SAILOR <muhammetuzunoglu@gmail.com>
2024-12-30 14:32:39 +0300

    4. commit

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,16 +1,17 @@
-
-import DashBoardTable from "../../pages/DashboardPage/DashBoardTable"
-import ButtonAddTransactions from "../../components/ButtonAddTransactions/ButtonAddTransactions"
-import Sidebar from "../../components/Sidebar/Sidebar"
+import DashBoardTable from '../../pages/DashboardPage/DashBoardTable';
+import ButtonAddTransactions from '../../components/ButtonAddTransactions/ButtonAddTransactions';
+import Sidebar from '../../components/Sidebar/Sidebar';
 
 const DashboardPage = () => {
-    return (
-        <div className="flex flex-col desktop:flex-row">
-            <Sidebar />
-            <DashBoardTable />
-            <ButtonAddTransactions />
-        </div>
-    )
-}
+  return (
+    <div className="flex min-h-screen">
+      <div className="w-[37.5%]">
+        <Sidebar />
+      </div>
+      <DashBoardTable />
+      <ButtonAddTransactions />
+    </div>
+  );
+};
 
 export default DashboardPage;
--- src/redux/balanceSlice.js
+++ src/redux/balanceSlice.js
@@ -1,44 +1,25 @@
-import { Line } from 'react-chartjs-2';
-import 'chart.js/auto';
+import { createSlice } from '@reduxjs/toolkit';
 
-const Chart = ({ chartData }) => {
-  const hasData = chartData && chartData.length > 0;
-
-  const labels = hasData ? chartData.map((data) => data.label) : ['No Data'];
-  const dataValues = hasData ? chartData.map((data) => data.value) : [50];
-
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: 'Currency Trend',
-        data: dataValues,
-        borderColor: '#FF6384',
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        fill: true,
-        tension: 0.4,
-        pointBackgroundColor: '#FF6384',
-        pointBorderColor: '#FF6384',
-      },
-    ],
-  };
-
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: { display: false },
-    },
-    scales: {
-      x: { ticks: { color: '#fff' } },
-      y: { ticks: { color: '#fff' } },
-    },
-  };
-
-  return (
-    <div className="max-w-md mx-auto">
-      <Line data={data} options={options} />
-    </div>
-  );
+const initialState = {
+  balance: 24000,
+  currency: [
+    { name: 'USD', purchase: 27.55, sale: 27.65 },
+    { name: 'EUR', purchase: 30.0, sale: 30.1 },
+  ],
 };
 
-export default Chart;
+const balanceSlice = createSlice({
+  name: 'balance',
+  initialState,
+  reducers: {
+    updateBalance(state, action) {
+      state.balance = action.payload;
+    },
+    updateCurrency(state, action) {
+      state.currency = action.payload;
+    },
+  },
+});
+
+export const { updateBalance, updateCurrency } = balanceSlice.actions;
+export default balanceSlice.reducer;
