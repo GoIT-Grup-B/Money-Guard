d5f00405dc10809c1877c56a9deabf08598783ee
gizemcoban <gizem.coban.r@gmail.com>
2024-12-30 23:27:31 +0300

    react import deleted

--- src/pages/DashboardPage/DashBoardTable.jsx
+++ src/pages/DashboardPage/DashBoardTable.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
 import './DashBoardTable.module.css';
 import { useDispatch, useSelector } from 'react-redux';
