fe644e7c39c453634554d2e3dec25d198a5669bc
gizemcoban <gizem.coban.r@gmail.com>
2025-01-01 13:47:57 +0300

    aside flex-2

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -5,12 +5,12 @@ import { Outlet } from 'react-router-dom';
 
 const DashboardPage = () => {
   return (
-    <div className="tablet:flex tablet:flex-column desktop:flex-row mobile:flex-col mobile:gap-[5px] mobile:overflow-y-auto">
-      <aside>
-        <Sidebar />
+    <div className="flex tablet:flex tablet:flex-column desktop:flex-row mobile:flex-col mobile:gap-[5px] mobile:overflow-y-auto">
+      <aside className="flex-[2]">
+        <Sidebar  />
       </aside>
 
-      <div className="flex flex-col p-6">
+      <div className="flex flex-[1] flex-col p-6">
         <DashBoardTable />
         <div className="flex-1">
           <Outlet />
