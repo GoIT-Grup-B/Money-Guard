f24c432059dbc5aca682c6a3fb89edb2ddf7b808
Heza GÖLCÜK <h.golcuk91@outlook.com>
2025-01-02 16:50:21 +0300

    scrol

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -2,12 +2,11 @@ import DashBoardTable from '../../pages/DashboardPage/DashBoardTable';
 import ButtonAddTransactions from '../../components/ButtonAddTransactions/ButtonAddTransactions';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import { Outlet } from 'react-router-dom';
-import Statistics from '../../components/Statistics/Statistics';
 
 const DashboardPage = () => {
     return (
         <div className="flex tablet:flex-column desktop:flex-row mobile:flex-col mobile:gap-[5px] mobile:overflow-y-auto">
-            <aside className="flex-[0.7]">
+            <aside className="overscroll-auto flex-[0.7]">
                 <Sidebar />
             </aside>
 
